c6443c2f9790302c96ea080d4e326dc612789214
Sergey Pudov <pellexa@gmail.com>
2023-01-15 18:32:53 +0300

    Tab size is set to 2 in VSCode settings.

--- src/index.ts
+++ src/index.ts
@@ -32,16 +32,16 @@ app.innerHTML = routes[window.location.pathname]
 
 const pagesHTML = `
 <nav class="tmp-nav">
-    <ul class="tmp-pages">
-        <li><a href="/singin">Singin</a></li>
-        <li><a href="/registration">Registration</a></li>
-        <li><a href="/chat">Chat</a></li>
-        <li><a href="/profile/detailed">Profile detailed</a></li>
-        <li><a href="/profile/edit">Profile edit</a></li>
-        <li><a href="/profile/password/edit">Profile password edit</a></li>
-        <li><a href="/404">404</a></li>
-        <li><a href="/500">500</a></li>
-    </ul>
+  <ul class="tmp-pages">
+    <li><a href="/singin">Singin</a></li>
+    <li><a href="/registration">Registration</a></li>
+    <li><a href="/chat">Chat</a></li>
+    <li><a href="/profile/detailed">Profile detailed</a></li>
+    <li><a href="/profile/edit">Profile edit</a></li>
+    <li><a href="/profile/password/edit">Profile password edit</a></li>
+    <li><a href="/404">404</a></li>
+    <li><a href="/500">500</a></li>
+  </ul>
 </nav>
 `
 
