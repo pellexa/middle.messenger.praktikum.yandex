fa65ad360473b59b66d8e4cf5a79f1f3992e6caa
Sergey Pudov <pellexa@gmail.com>
2023-01-16 10:54:49 +0300

    ESLint fixed server.js.

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.use('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
 
-app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
-}); 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+})
