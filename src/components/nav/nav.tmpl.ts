ed197be2ee63a3e009d00a463ae18ebe75ff832c
Sergey Pudov <pellexa@gmail.com>
2023-01-30 09:06:19 +0300

    Nav is now a component.

---
+++ src/components/nav/nav.tmpl.ts
@@ -0,0 +1,14 @@
+import './nav.scss'
+
+export default `
+<ul class="tmp-pages">
+  <li><a href="/signin">Signin</a></li>
+  <li><a href="/registration">Registration</a></li>
+  <li><a href="/chat">Chat</a></li>
+  <li><a href="/profile/detailed">Profile detailed</a></li>
+  <li><a href="/profile/edit">Profile edit</a></li>
+  <li><a href="/profile/password/edit">Profile password edit</a></li>
+  <li><a href="/404">404</a></li>
+  <li><a href="/500">500</a></li>
+</ul>
+`
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ import registration from './pages/registration'
 import error404 from './pages/error/404'
 import error500 from './pages/error/5xx'
 // import main from './pages/main'
-import profileDetailed from './pages/profile/profileDetailed'
+// import profileDetailed from './pages/profile/profileDetailed'
 import profileEdit from './pages/profile/profileEdit'
 import profilePasswordEdit from './pages/profile/profilePasswordEdit'
 // import profileUploadAvatar from './pages/profile/profileUploadAvatar'
 import { render } from './utils/renderDOM'
 import Block from './modules/block'
+import nav from './components/nav'
 
 const app = document.getElementById('app')
 
@@ -23,28 +24,12 @@ const routes: Record<string, Block> = {
   '/': registration,
   '/registration': registration,
   // '/chat': main,
-  '/profile/detailed': profileDetailed,
+  // '/profile/detailed': profileDetailed,
   '/profile/edit': profileEdit,
   '/profile/password/edit': profilePasswordEdit,
   '/404': error404,
   '/500': error500,
 }
 
-render('#app', routes[window.location.pathname])
-
-const pagesHTML = `
-<nav class="tmp-nav">
-  <ul class="tmp-pages">
-    <li><a href="/signin">Signin</a></li>
-    <li><a href="/registration">Registration</a></li>
-    <li><a href="/chat">Chat</a></li>
-    <li><a href="/profile/detailed">Profile detailed</a></li>
-    <li><a href="/profile/edit">Profile edit</a></li>
-    <li><a href="/profile/password/edit">Profile password edit</a></li>
-    <li><a href="/404">404</a></li>
-    <li><a href="/500">500</a></li>
-  </ul>
-</nav>
-`
-
-app.innerHTML += pagesHTML
+const root = render('#app', routes[window.location.pathname])
+root!.append(nav.getContent())
