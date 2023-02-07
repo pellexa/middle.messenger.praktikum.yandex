85119f49bd65ff325cb7adb7b2c629ba4f1d9767
Sergey Pudov <pellexa@gmail.com>
2023-02-07 16:15:53 +0300

    Error pages now uses Router.

--- src/pages/error/404/index.ts
+++ src/pages/error/404/index.ts
@@ -1,6 +1,34 @@
+import Link from '../../../components/link'
 import TheError from '../../../components/theError'
+import Router from '../../../modules/Router'
 
-const error404HTML = new TheError(
+const linkBack = new Link(
+  'a',
+  {
+    tagAttrs: {
+      class: 'error-box__link link_color_blue',
+      href: '/messanger',
+    },
+    content: 'назад к чатам',
+    events: {
+      click: (event: Event) => {
+        event.preventDefault()
+
+        const element = event.target as HTMLLinkElement
+        const router = Router.getInstance()
+        const uri = element.getAttribute('href')
+
+        if (!uri) {
+          throw new Error('The href attribute must exist on the "a" tag.')
+        }
+
+        router.go(uri)
+      },
+    },
+  }
+)
+
+const error404 = new TheError(
   'main',
   {
     tagAttrs: {
@@ -9,12 +37,9 @@ const error404HTML = new TheError(
     error: {
       status: 404,
       text: 'не туда попали',
-      back: {
-        link: '#',
-        text: 'назад к чатам',
-      },
     },
+    linkBack,
   }
 )
 
-export default error404HTML
+export default error404
