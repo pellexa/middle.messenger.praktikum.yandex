d69e6ce02dd743e7ded2db7f58e6b2e7328625ce
Sergey Pudov <pellexa@gmail.com>
2023-02-16 13:03:34 +0300

    WIP. SigninPage now uses a controller.

--- src/pages/signin/index.ts
+++ src/pages/signin/index.ts
@@ -3,11 +3,12 @@ import Button from '../../components/button'
 import Input from '../../components/input'
 import Block from '../../modules/block'
 import { SigninProps } from './types'
-import { jsonFromData, runValidation, validationFormData } from '../../utils/formUtils'
+import { runValidation } from '../../utils/formUtils'
 import ValidationError from '../../components/validationError'
 import Label from '../../components/label'
 import Link from '../../components/link'
 import Router from '../../modules/Router'
+import AuthController from '../../controllers/auth'
 
 const acceptButton = new Button(
   'button',
@@ -137,7 +138,7 @@ class SigninPage extends Block {
   }
 }
 
-const signinHTML = new SigninPage('main', {
+const signin = new SigninPage('main', {
   tagAttrs: {
     class: 'form-box',
   },
@@ -166,17 +167,10 @@ const signinHTML = new SigninPage('main', {
         },
       ]
 
-      const validationResults = validationFormData.call(event, fields)
-      const result = Object.values(validationResults).every((value: boolean) => value === true)
-
-      const json = jsonFromData.call(event, fields)
-      console.log('json: ', json)
-
-      if (result) {
-        console.log('send api request')
-      }
+      const authController = new AuthController(event, fields)
+      authController.signin()
     },
   },
 })
 
-export default signinHTML
+export default signin
